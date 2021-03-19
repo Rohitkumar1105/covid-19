4d9ad866e768abbe4e97986df07e1912bb5a8438
Rohit Kumar <=>
2021-03-19 16:48:49 +0530

    removed the error boundary

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import React, { useEffect } from 'react'
-import { Sidenav, Cards, ErrorBoundary } from './components'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Provider, useDispatch } from 'react-redux'
-import store from './store'
+import React, { useEffect } from "react";
+import { Sidenav, Cards } from "./components";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Provider, useDispatch } from "react-redux";
+import store from "./store";
 
-import { fetchData, fetchPastData } from './action/getCoronaData_action'
+import { fetchData, fetchPastData } from "./action/getCoronaData_action";
 
 const AppWrapper = () => {
   return (
     <Provider store={store}>
       <App />
     </Provider>
-  )
-}
+  );
+};
 
 const App = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchData())
-    dispatch(fetchPastData())
-  }, [dispatch])
+    dispatch(fetchData());
+    dispatch(fetchPastData());
+  }, [dispatch]);
 
   return (
-    <ErrorBoundary>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Sidenav} />
-          <Route exact path={`${process.env.PUBLIC_URL}/:name`} component={Cards} />
-        </Switch>
-      </BrowserRouter>
-    </ErrorBoundary>
-  )
-}
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Sidenav} />
+        <Route
+          exact
+          path={`${process.env.PUBLIC_URL}/:name`}
+          component={Cards}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-export default AppWrapper
+export default AppWrapper;
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
-export { default as Sidenav } from './Sidenav'
-export { default as Cards } from './Cards'
-export { default as Graph } from './Graph'
-export { default as Home } from './Home'
-export { default as Header } from './Header'
-export { default as ErrorBoundary } from './ErrorBoundary'
+export { default as Sidenav } from "./Sidenav";
+export { default as Cards } from "./Cards";
+export { default as Graph } from "./Graph";
+export { default as Home } from "./Home";
+export { default as Header } from "./Header";
